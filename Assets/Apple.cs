039c0a16186009bc820e7c9a3d8126ae7c1abb1d
Martin Aranas <msa139@scarletmail.rutgers.edu>
2016-02-25 06:11:01 -0500

    Update 2/25

--- Assets/Apple.cs
+++ Assets/Apple.cs
@@ -2,14 +2,16 @@
 using System.Collections;
 
 public class Apple : MonoBehaviour {
+	public static float bottomY = -20f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
 	void Update () {
-	
+		if (transform.position.y < bottomY) {
+			Destroy (this.gameObject);
+
+			// Get a reference to the ApplePicker component of Main Camera
+			ApplePicker apScript = Camera.main.GetComponent<ApplePicker>(); //1
+			// Call the public AppleDestroyed() method of apScript
+			apScript.AppleDestroyed();
+		}
 	}
 }
--- Assets/Basket.cs
+++ Assets/Basket.cs
@@ -3,13 +3,45 @@ using System.Collections;
 
 public class Basket : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
+	public GUIText		scoreGT;
+
 	void Update () {
-	
+		// Get the current screen position of the mouse from Input
+		Vector3 mousePos2D = Input.mousePosition; //1
+
+		// The Camera's z position sets the how far to push the mouse into 3D
+		mousePos2D.z = -Camera.main.transform.position.z; //2
+
+		// Convert the point from 2D screen space into 3D game world space
+		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint ( mousePos2D ) ; // 3
+
+		// Move the x position of this Basket to the x position of the Mouse
+		Vector2 pos = this.transform.position;
+		pos.x = mousePos3D.x;
+		this.transform.position = pos;
+	}
+
+	void Start() {
+		// Find a reference to the ScoreCounter GameObject
+		GameObject scoreGO = GameObject.Find ("ScoreCounter");
+		// Get the GUIText Component of that GameObject
+		scoreGT = scoreGO.GetComponent<GUIText> ();
+		// Set the starting number of points to 0
+		scoreGT.text = "0";
+	}
+
+	void OnCollisionEnter ( Collision coll ) {
+		//Find out what his this basket
+		GameObject collidedWith = coll.gameObject;
+		if (collidedWith.tag == "Apple") {
+			Destroy (collidedWith);
+		}
+
+		// Parse the text of the scoreGT into an int
+		int score = int.Parse(scoreGT.text);
+		// Add points for catching the apple
+		score += 100;
+		// Convert the score back to a string and display it
+		scoreGT.text = score.ToString();
 	}
 }
